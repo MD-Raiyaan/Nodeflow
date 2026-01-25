bd6ab963a02898992d6b6ca41ab5c3320e40f82a
Mohammed Raiyaan <Mohammedraiyaan2005@gmail.com>
2026-01-25 17:52:19 +0530

    node execution

--- src/features/executions/components/http-request/node.tsx
+++ src/features/executions/components/http-request/node.tsx
@@ -4,13 +4,12 @@ import { useReactFlow, type Node, type NodeProps } from "@xyflow/react";
 import { GlobeIcon } from "lucide-react";
 import { memo, useState } from "react";
 import { BaseExecutionNode } from "../base-execution-node";
-import { formType, HTTPRequestDialog } from "./dialog";
+import { HttpRequestFormValues, HTTPRequestDialog } from "./dialog";
 
 type HttpRequestNodeData = {
   endpoint?: string;
   method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
   body?: string;
-  [key: string]: unknown;
 };
 
 type HttpRequestNodeType = Node<HttpRequestNodeData>;
@@ -25,22 +24,22 @@ export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
   const handleOpenSettings = () => setDialogOpen(true);
   const {setNodes}=useReactFlow();
 
-  const handleSubmit=(values:formType)=>{
-     setNodes((nodes)=>nodes.map((node)=>{
-        if(node.id===props.id){
-            return{
-                ...node,
-                data:{
-                    ...node.data,
-                    endpoint:values.endpoint,
-                    method:values.method,
-                    body:values.body,
-                }
-            }
+  const handleSubmit = (values: HttpRequestFormValues) => {
+    setNodes((nodes) =>
+      nodes.map((node) => {
+        if (node.id === props.id) {
+          return {
+            ...node,
+            data: {
+              ...node.data,
+              ...values,
+            },
+          };
         }
         return node;
-     }))
-  }
+      }),
+    );
+  };
 
 
   return (
@@ -49,9 +48,7 @@ export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
         open={diaglogOpen}
         onOpenChange={setDialogOpen}
         onSubmit={handleSubmit}
-        defaultEndpoint={nodeData.endpoint}
-        defaultMethod={nodeData.method}
-        defaultBody={nodeData.body}
+        defaultValues={nodeData}
       />
       <BaseExecutionNode
         {...props}
---
+++ src/inngest/utils.ts
@@ -0,0 +1,44 @@
+import { Connection, Node } from "@/generated/prisma/client";
+import toposort from "toposort";
+
+export const topologicalSort=(nodes:Node[],
+    connections:Connection[]
+):Node[]=>{
+    // if no connections, return nodes as is 
+    if(connections.length==0){
+       return nodes;
+    }
+
+    const edges:[string,string][]=connections.map((conn)=>[
+        conn.fromNodeId,
+        conn.toNodeId,
+    ]);
+
+    // add nodes with no connections as self edges to ensure they are included
+    const connectedNodesIds=new Set<string>();
+    for(const conn of connections){
+        connectedNodesIds.add(conn.fromNodeId);
+        connectedNodesIds.add(conn.toNodeId);
+    }
+     
+    for (const node of nodes){
+        if(!connectedNodesIds.has(node.id)){
+            edges.push([node.id,node.id]);
+        }
+    }
+
+    // perform sort
+    let sortedNodeIds:string[];
+    try{
+        sortedNodeIds=toposort(edges);
+        sortedNodeIds=[...new Set(sortedNodeIds)];
+    }catch(error){
+        if(error instanceof Error && error.message.includes("Cyclic")){
+            throw new Error("workflow contains a cycle");
+        }
+        throw error;
+    }
+
+    const nodeMap=new Map(nodes.map((n)=>[n.id,n]));
+    return sortedNodeIds.map((id)=>nodeMap.get(id)!).filter(Boolean);
+}
